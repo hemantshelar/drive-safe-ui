dbdcd7d0b925312c0795b06360d3400c086753db
Hemant <hemant.shelar@gmail.com>
2025-04-29 17:47:15 +1000

    Initial checking

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { MainBodyComponent } from './main-body/main-body.component';
+
+export const routes: Routes = [
+    {path: '', component: MainBodyComponent }
+];
